eb1616dd462bca5adc03e97735c2c4e5362c4ee1
StruzhakS <StruzhakS@gmail.com>
2023-06-04 13:00:03 +0300

    Vykonav domashnye zavdannya 4-1 na khukakh 37 / 5 000 Результати перекладу Результат перекладу Did my homework 4-1 on hooks

--- src/components/Feedback/feedback.jsx
+++ src/components/Feedback/feedback.jsx
@@ -1,35 +1,33 @@
-import { Component } from 'react';
+import { useState } from 'react';
 import s from './Feedback.module.css';
 import Statistics from 'components/Statistics/Statistics';
 import FeedbackOptions from 'components/OnLeaveFeedback/FeedbackOptions';
 
-class Feedback extends Component {
-  state = {
-    good: 0,
-    neutral: 0,
-    bad: 0,
+function Feedback() {
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+  const onLeaveFeedback = value => {
+    if (value === 'good') {
+      return setGood(good + 1);
+    }
+    if (value === 'neutral') {
+      return setNeutral(neutral + 1);
+    }
+    if (value === 'bad') {
+      return setBad(bad + 1);
+    }
   };
 
-  onLeaveFeedback = value => {
-    return this.setState(prevstate => ({
-      [value]: prevstate[value] + 1,
-    }));
-  };
-
-  render() {
-    const { good, neutral, bad } = this.state;
-    console.log(good);
-
-    return (
-      <>
-        <div>
-          <h2 className={s.title}>PLease leave feedback ;) </h2>
-          <FeedbackOptions onLeaveFeedback={this.onLeaveFeedback} />
-          <Statistics good={good} neutral={neutral} bad={bad} />
-        </div>
-      </>
-    );
-  }
+  return (
+    <>
+      <div>
+        <h2 className={s.title}>PLease leave feedback ;) </h2>
+        <FeedbackOptions onLeaveFeedback={onLeaveFeedback} />
+        <Statistics good={good} neutral={neutral} bad={bad} />
+      </div>
+    </>
+  );
 }
 
 export default Feedback;
